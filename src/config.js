1005a693b1011fc299ff4c46881284ca07e647d7
Daniel Tostes <daniel.tostes@mercadolivre.com>
2020-12-16 19:25:01 -0300

    Impl benchmark tests

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     comparsionTimes: 3,
-    furyToken: "9649fa0cc80654e8e55377101ab234d1ee85effb99b76a0eac328572023abe69",
+    furyToken: "",
     goHost: "https://test_taxes-go-benchmark.furyapps.io",
-    javaHost: "https://test_taxes-java-benchmark.furyapps.io/",
+    javaHost: "https://test_taxes-java-benchmark.furyapps.io",
     healthPath: "/ping",
     benchmarkPath: "/benchmark"
 }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,85 @@
 const { default: Axios } = require("axios");
 const { javaHost, goHost, healthPath, furyToken, comparsionTimes } = require("./config");
 
-function createComparsion(path, javaTime, goTime) {
-  return { path, responseTimeJavaMs: `${javaTime}ms`, responseTimeGoMs: `${goTime}ms` }
+function createComparsion(path, javaTime, goTime, javaStatus, goStatus) {
+  return { path, responseTimeJavaMs: javaTime, responseTimeGoMs: goTime, responseStatusJava: javaStatus, responseStatusGo: goStatus }
 }
 
 async function doBenchmark(path) {
-  const headers = {
-    'x-auth-token': furyToken
-  }
-
-  comparsion = {
-    avg: 0,
-    comparsions: []
-  }
   let totalJava = 0
   let totalGo = 0
-  for (let i = 0; i < comparsionTimes; i++) {
-    const javaStart = new Date().getTime()
-    await Axios.get(javaHost + path, { headers })
-    const javaDuration = new Date().getTime() - javaStart
-    totalJava += javaDuration
+  const [javaResponse, goResponse] = await Promise.all([
+    get(javaHost, path),
+    get(goHost, path)
+  ])
+  totalJava += javaResponse.duration
+  totalGo += goResponse.duration
 
-    const goStart = new Date().getTime()
-    await Axios.get(goHost + path, { headers })
-    const goDuration = new Date().getTime() - goStart
-    totalGo += goDuration
-
-    comparsion.comparsions.push(createComparsion(path, javaDuration, goDuration))
-  }
-  comparsion.avg = createComparsion(path, totalJava / comparsionTimes, totalGo / comparsionTimes)
-
-  return comparsion
+  return createComparsion(path, javaResponse.duration, goResponse.duration, javaResponse.status, goResponse.status)
 }
 
 (async function doComparsion() {
-  console.log("Beginning comparsion")
-  console.log(`javaHost=${javaHost}, goHost=${goHost}, healthPath=${healthPath}, furyToken=${furyToken}, comparsionTimes=${comparsionTimes}`)
+  console.log("Beginning comparsion...")
+  console.table({ javaHost, goHost, healthPath, furyToken, comparsionTimes })
   try {
     await doBenchmark(healthPath)
-    console.log("Health checked")
-    const [healthBenchmark] = await Promise.all([
-      doBenchmark(healthPath)
+    console.log("Executing in serial...")
+    healthResponse = await doBenchmark(healthPath);
+    size10Response = await doBenchmark('/benchmark?times=10');
+    size100Response = await doBenchmark('/benchmark?times=100');
+    size1000Response = await doBenchmark('/benchmark?times=1000');
+    size10000Response = await doBenchmark('/benchmark?times=10000');
+    console.table([
+      healthResponse,
+      size10Response,
+      size100Response,
+      size1000Response,
+      size10000Response,
     ])
-    console.table([healthBenchmark.avg])
-    console.table(healthBenchmark.comparsions)
+    console.log("Executing in parallel...")
+    const [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40] = await Promise.all([
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+      doBenchmark('/benchmark?times=10000'),
+      doBenchmark('/benchmark?times=100000'),
+    ])
+    console.table([r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40]);
   } catch (error) {
     console.error(error.stack)
     console.error("Comparsion could not be done")
@@ -53,4 +88,21 @@ async function doBenchmark(path) {
   console.log("Comparsion successfully done")
   return 0
 })()
+
+async function get(host, path) {
+  const headers = {
+    'x-auth-token': furyToken
+  }
+  let status = 'ok'
+  const start = new Date().getTime();
+  try {
+    await Axios.get(host + path, { headers });
+  } catch (error) {
+    console.error(`problem=${host + path}`);
+    console.error(error.stack)
+    status = 'error'
+  }
+  const duration = new Date().getTime() - start;
+  return { duration, status };
+}
 
